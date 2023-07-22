dc5780ff5334859b2ff1fe5cfb56f5fe58383a07
nurali0709 <imusk793@gmail.com>
2023-07-23 01:31:24 +0500

    added formatted date and post style

--- src/components/PostBlock.vue
+++ src/components/PostBlock.vue
@@ -45,29 +45,74 @@
           });
       },
       formatDate(date) {
-        if (!date) {
+      if (!date) {
         return 'N/A'; // Return a fallback value when the date is not available
       }
-        // Add a method to format the date as you desire
-        // For example, you can use a library like moment.js
-        // or write a custom function to format the date
-        // This example assumes the date format is "YYYY-MM-DD"
-        const parts = date.split('-');
-        return `${parts[2]}/${parts[1]}/${parts[0]}`;
-      },
+      // Split the date into parts using "-" as the separator
+      const parts = date.split('-');
+      const year = parts[0];
+      const month = this.getMonthName(parseInt(parts[1]));
+      const day = parts[2];
+      return `${day} ${month} ${year}`;
     },
-  };
-  </script>
+    getMonthName(monthNumber) {
+      // Convert month number (1 to 12) to the corresponding month name
+      const monthNames = [
+        'January', 'February', 'March', 'April', 'May', 'June',
+        'July', 'August', 'September', 'October', 'November', 'December'
+      ];
+      return monthNames[monthNumber - 1];
+    },
+  },
+};
+</script>
   
-  <style>
-  .post-block {
-    /* Styles for the post block container */
-  }
-  
-  .post {
-    /* Styles for individual post cards */
-  }
-  
-  /* Add other CSS styles as needed */
-  </style>
+<style>
+.post-block {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  margin: 20px 0;
+}
+
+.post {
+  background-color: #f5f5f5;
+  border-radius: 8px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  width: calc(33.33% - 20px); 
+  margin: 10px;
+  padding: 10px;
+  flex: 1 0 auto; /* Allow the post to grow to occupy the entire row width */
+  max-width: 300px; 
+}
+
+.post-header {
+  margin-bottom: 10px;
+}
+
+.post-meta {
+  font-size: 14px;
+  color: #777;
+}
+
+.post-content {
+  font-size: 16px;
+  line-height: 1.6;
+}
+
+.post-footer {
+  margin-top: 20px;
+  border-top: 1px solid #ddd;
+  padding-top: 10px;
+}
+
+.post-stats {
+  font-size: 14px;
+  color: #555;
+}
+
+.post-stats span {
+  margin-right: 20px;
+}
+</style>
   
