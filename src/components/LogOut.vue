9affc926ec424624a840f69a4cea42b52346580f
nurali0709 <imusk793@gmail.com>
2023-08-02 19:09:07 +0500

    added logout func(with 401)

--- src/components/LogOut.vue
+++ src/components/LogOut.vue
@@ -6,28 +6,19 @@
 </template>
   
 <script>
-import axios from 'axios';
-
-  export default {
-    methods: {
-      logout() {
-        console.log('Token:', this.$cookies.get('access_token'));
-        // Call the backend API to logout and delete the token cookie
-        axios
-          .post("http://127.0.0.1:8000/auth/logout")
-          .then(() => {
-            // Remove the token from the cookie
-            console.log('Before Removal');
-            this.$cookies.remove("access_token");
-            console.log('After Removal');
-            // Redirect to the login page or any other public page
-            this.$router.push("/login");
-          })
-          .catch((error) => {
-            console.error(error);
-            // Handle logout failure (e.g., show an error message)
-          });
-      },
+export default {
+  methods: {
+    async logout() {
+      try {
+        await this.$store.dispatch("logout"); // Call the logout action in the store
+        this.$cookies.remove("access_token"); // Remove the access token from the cookie
+        // Redirect to the login page or any other public page
+        this.$router.push("/login");
+      } catch (error) {
+        console.error(error);
+        // Handle logout failure (e.g., show an error message)
+      }
     },
-  };
+  },
+};
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,25 @@
 import { createStore } from 'vuex';
+import axios from 'axios';
 
 const store = createStore({
   state: {
-    isLoggedIn: false, // Initialize isLoggedIn as false by default
+    isLoggedIn: false,
   },
   mutations: {
-    // A mutation to update the isLoggedIn state
     updateLoggedInStatus(state, isLoggedIn) {
       state.isLoggedIn = isLoggedIn;
+    },
+  },
+  actions: {
+    // An action to perform logout and update isLoggedIn state
+    async logout({ commit }) {
+      try {
+        await axios.post("http://127.0.0.1:8000/auth/logout");
+        commit("updateLoggedInStatus", false); // Update isLoggedIn to false in the store
+      } catch (error) {
+        console.error(error);
+        // Handle logout failure (e.g., show an error message)
+      }
     },
   },
 });
