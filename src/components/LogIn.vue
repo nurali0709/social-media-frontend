36850300d416cb8158a427386e3ae9c44516aa3e
nurali0709 <imusk793@gmail.com>
2023-08-02 18:40:34 +0500

    styled login

--- src/components/LogIn.vue
+++ src/components/LogIn.vue
@@ -1,7 +1,6 @@
 <template>
-    <div>
-      <h1>Login</h1>
-      <form @submit.prevent="login">
+    <div class="login-container">
+      <form @submit.prevent="login" class="login-form">
         <label for="username">Username</label>
         <input type="text" id="username" v-model="user.username" required>
         <br>
@@ -44,4 +43,62 @@ import axios from 'axios';
     },
   };
 </script>
+
+
+<style>
+.login-container {
+  max-width: 400px;
+  margin: 0 auto;
+  margin-top: -100px;
+  padding: 20px;
+  background-color: #f1f1f1;
+  border-radius: 5px;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
+}
+
+.login-form label {
+  display: block;
+  font-weight: bold;
+  margin-bottom: 5px;
+  color: #555;
+  margin-top: 10px;
+}
+
+.login-form input[type="text"],
+.login-form input[type="password"] {
+  width: 100%;
+  padding: 10px;
+  margin-bottom: 15px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+
+.login-form button {
+  width: 100%;
+  padding: 10px;
+  background-color: #176B87;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 18px;
+}
+
+.login-form button:hover {
+  background-color: #DAFFFB;
+  color: #176B87;
+}
+
+.login-form button:focus {
+  outline: none;
+}
+
+/* Center the form vertically */
+.login-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+}
+</style>
   
