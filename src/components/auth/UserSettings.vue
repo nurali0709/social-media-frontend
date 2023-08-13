8a5d3606e541d700127fcb67da39ce9991f5486d
nurali0709 <imusk793@gmail.com>
2023-08-14 02:08:34 +0500

    styled user settings component

--- src/components/auth/UserSettings.vue
+++ src/components/auth/UserSettings.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="user-settings">
       <h2>User Settings</h2>
-      <form @submit.prevent="updateUser">
+      <form @submit.prevent="updateUser" class="settings-form">
         <label for="username">Username</label>
         <input type="text" id="username" v-model="userData.username" required>
         <br>
@@ -14,7 +14,7 @@
         <label for="surname">Surname</label>
         <input type="text" id="surname" v-model="userData.surname" required>
         <br>
-        <button type="submit">Update</button>
+        <button type="submit" class="update-button">Update</button>
       </form>
     </div>
 </template>
@@ -66,4 +66,55 @@
     },
   };
 </script>
+
+<style scoped>
+.user-settings {
+  max-width: 600px;
+  margin: auto;
+  padding: 30px;
+  background-color: #f5f5f5;
+  border-radius: 8px;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+.user-settings h2 {
+  margin-bottom: 20px;
+  font-size: 24px;
+  color: #333;
+}
+
+.settings-form {
+  display: flex;
+  flex-direction: column;
+}
+
+.settings-form label {
+  font-size: 16px;
+  margin-bottom: 5px;
+  color: #555;
+}
+
+.settings-form input {
+  padding: 8px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  margin-bottom: 15px;
+  font-size: 14px;
+}
+
+.settings-form button.update-button {
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  border-radius: 4px;
+  font-size: 16px;
+  cursor: pointer;
+  transition: background-color 0.3s ease-in-out;
+}
+
+.settings-form button.update-button:hover {
+  background-color: #0056b3;
+}
+</style>
   
